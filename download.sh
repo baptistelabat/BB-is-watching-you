7c2c73cedd68e93c08522b07d8205572429aca83
baptistelabat <baptiste.labat@gmail.com>
2015-02-12 11:09:29 +0100

    Adding download file and gitignore

---
+++ download.sh
@@ -0,0 +1,13 @@
+wget -q -U Mozilla -O output0.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=          hello+fanny, how are you doing?"
+
+wget -q -U Mozilla -O output1.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=          how was your party?"
+
+wget -q -U Mozilla -O output2.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=          Baptiste went to bed. He is probably sleeping"
+
+wget -q -U Mozilla -O output3.mp3 "http://translate.google.com/translate_tts?ie=UTF-8&tl=en&q=          Have a good night!"
+
+
+
+
+
+
